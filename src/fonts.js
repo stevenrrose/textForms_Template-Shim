ee1b17c9e77e29656e670b545326ede00fe28ee3
Frédéric Bonnet <f.bonnet@creative-ingenierie.fr>
2015-03-31 22:57:40 +0200

    Moved font list into fonts.js

---
+++ src/fonts.js
@@ -0,0 +1,9 @@
+$(this).on("load", function() {
+	loadFonts([
+		"fonts/MyriadPro.otf", 
+		"fonts/Braggadocio.ttf",
+		"fonts/Geneva.ttf",
+		"fonts/GillSans-UtraBold.ttf",
+		"fonts/Impact.ttf",
+	]);
+});
